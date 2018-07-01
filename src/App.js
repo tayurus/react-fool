137900670e5bd2fe87464f61d9aee214c13ad5f5
tayurus <tayurus@gmail.com>
2018-07-01 12:51:39 +0300

    formating existing code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import { Enter } from './Components/Enter/Enter.js';
-import './App.css';
+import React, { Component } from "react";
+import { Enter } from "./Components/Enter/Enter.js";
+import "./App.css";
 
 class App extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {logged: false}
+        this.state = { logged: false };
 
         this.enter = this.enter.bind(this);
     }
 
-    enter(){
-        this.setState({logged:true})
+    enter() {
+        this.setState({ logged: true });
     }
 
-
-  render() {
-    return (
-      <div className="App">
-          {this.state.logged ? "" : <Enter enter={this.enter}></Enter>}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                {this.state.logged ? "" : <Enter enter={this.enter} />}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/Enter/Enter.js
+++ src/Components/Enter/Enter.js
@@ -1,39 +1,46 @@
-import React, { Component } from 'react';
-import './Enter.css';
+import React, { Component } from "react";
+import "./Enter.css";
 
-export class Enter extends React.Component{
-    constructor(props){
-        super(props)
+export class Enter extends React.Component {
+    constructor(props) {
+        super(props);
         this.handleEnterClick = this.handleEnterClick.bind(this);
-        this.state = {message: ""}
+        this.state = { message: "" };
     }
 
-    handleEnterClick(){
+    handleEnterClick() {
         let reg = /^[a-z]{1,}.{0,}?$/;
         let userInput = this.refs.nickname.value;
-        if (!reg.test(userInput)){
-            if (userInput.length == 0){
-                this.setState({message: "Enter your nickname!"});
-            }else {
-                this.setState({message: "Nickname must begin with letter!"});
+        if (!reg.test(userInput)) {
+            if (userInput.length == 0) {
+                this.setState({ message: "Enter your nickname!" });
+            } else {
+                this.setState({ message: "Nickname must begin with letter!" });
             }
+        } else {
+            this.setState({ message: "" });
+            this.props.enter();
         }
-        else {
-            this.setState({message: ""});
-            this.props.enter()
-        }
-
-
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="Enter">
                 <h2 className="Enter__title">Enter</h2>
-                <input type="text" ref="nickname" placeholder="Your nickname" className="Enter__input"/>
+                <input
+                    type="text"
+                    ref="nickname"
+                    placeholder="Your nickname"
+                    className="Enter__input"
+                />
                 <div className="Enter__message">{this.state.message}</div>
-                <button onClick={this.handleEnterClick} className="Enter__button">Let's go!</button>
+                <button
+                    onClick={this.handleEnterClick}
+                    className="Enter__button"
+                >
+                    Let's go!
+                </button>
             </div>
-        )
+        );
     }
 }
